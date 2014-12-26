edeaa9d581a3507e23dac4f8393d43c6699d111a
Michael Raimondi <mike@mikeraimondi.com>
2014-12-25 21:54:15 -0500

    Simplify handling of document body

--- frontmatter.go
+++ frontmatter.go
@@ -1,8 +1,6 @@
 package frontmatter
 
 import (
-	"io"
-	"io/ioutil"
 	"regexp"
 
 	"gopkg.in/yaml.v2"
@@ -10,35 +8,28 @@ import (
 
 const fmRegex = `(?ms)^\s*---.*---$`
 
-// Unmarshal parses the file containing YAML frontmatter and stores YAML encoded data in the value pointed to by v. The body of the document is written to w.
-func Unmarshal(data []byte, v interface{}, w io.Writer) (err error) {
+// Unmarshal parses the data containing YAML frontmatter and stores YAML encoded data in the value pointed to by v.
+func Unmarshal(data []byte, v interface{}) (rest []byte, err error) {
 	regex := regexp.MustCompile(fmRegex)
 	fmLoc := regex.FindIndex(data)
 	if fmLoc == nil {
-		_, err = w.Write(data)
-		return err
+		return data, nil
 	}
 	if err = yaml.Unmarshal(data[fmLoc[0]:fmLoc[1]], v); err != nil {
-		return err
+		return []byte{}, err
 	}
 	if len(data) <= fmLoc[1]+1 {
 		return
 	}
-	_, err = w.Write(data[fmLoc[1]+1:])
-	return err
+	return data[fmLoc[1]+1:], nil
 }
 
-// Marshal returns the YAML-frontmatter containing document from v. It appends the contents of r.
-func Marshal(v interface{}, r io.Reader) (data []byte, err error) {
+// Marshal returns the frontmatter encoding of v.
+func Marshal(v interface{}) (data []byte, err error) {
 	if data, err = yaml.Marshal(&v); err != nil {
-		return data, err
-	}
-	body, err := ioutil.ReadAll(r)
-	if err != nil {
 		return data, err
 	}
 	data = append([]byte("---\n"), data...)
 	data = append(data, []byte("---\n")...)
-	data = append(data, body...)
 	return data, nil
 }
